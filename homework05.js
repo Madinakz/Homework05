e0379622c6def4d94094633d2e575c9a401628a7
Madina Abilnazarova <madina@MacBook-Air-Madina.local>
2023-04-07 17:39:37 +0600

    first commit

---
+++ homework05.js
@@ -0,0 +1,15 @@
+document.getElementById('age-table')
+
+document.getElementsByTagName('label')
+HTMLCollection(4) [label, label, label, label]
+document.querySelector('td')
+​
+document.getElementsByName('search')
+NodeList(2) [form, input]
+document.getElementsByTagName('input')[0]
+
+document.querySelectorAll('input')
+NodeList(9) [input, input, input, input, input, input, input, input, input]0: 
+let input = document.querySelectorAll('input')
+undefined
+input[input.length-1]
